70f0d3c3460c91cff2a7a3d31007d252db4d393b
c1scok1d <rchatman@macinternetservices.com>
2022-01-17 23:56:49 -0600

    Initial commit

---
+++ lib/models/simple_geofence.dart
@@ -0,0 +1,39 @@
+import 'package:geofence_service/geofence_service.dart';
+import 'package:geofence_service/models/geofence_radius.dart';
+import 'package:geofence_service/models/geofence_status.dart';
+
+class SimpleGeofence {
+  String id;
+  double latitude;
+  double longitude;
+  String isfeatured, isPromotion;
+  String city_id;
+  String item_name, imageId;
+  List<GeofenceRadius> radius;
+  double expirationDuration;
+  GeofenceStatus transitionType;
+  int loiteringDelay = 60000;
+  bool isNear = false;
+
+  SimpleGeofence(
+      this.id,
+      this.latitude,
+      this.longitude,
+      this.isfeatured,
+      this.isPromotion,
+      this.city_id,
+      this.item_name,
+      this.imageId,
+      this.radius,
+      this.expirationDuration,
+      this.transitionType);
+
+  Geofence toGeofence() {
+    return Geofence(
+        latitude: latitude, longitude: longitude, radius: radius, id: id);
+  }
+
+  SimpleGeofence m() {
+    return this;
+  }
+}
